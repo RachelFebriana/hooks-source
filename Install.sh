d79b3b51b56125554de52265e2ea597e969cae38
Rachel Febriana <115395427+RachelFebriana@users.noreply.github.com>
2025-01-30 00:21:35 +0700

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -5,7 +5,8 @@ echo "Install Ndk Android"
 wget https://dl.google.com/android/repository/android-ndk-r27c-linux.zip
 sleep 5
 echo "Unzip Ndk File "
-mv  android-ndk-r27c-linux.zip $ACHEL
+unzip android-ndk-r27c-linux.zip
+mv  android-ndk-r27c-linux $ACHEL
 sleep 3
 echo "cleannn Build"
 rm -rf android-ndk-r27c-linux.zip
